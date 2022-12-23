2f39a3162a10b0d94604e66e71e088317f557483
tpdus001129 <tpdus001129@gamil.com>
2022-12-23 15:25:42 +0900

    12/22 RefEx

--- my-app/src/RefEx.js
+++ my-app/src/RefEx.js
@@ -44,4 +44,4 @@ export default RefEx;
 // 버튼 눌러도 텍스트상자에 포커싱  <의 해결방법
 // 변수 이름 정하고(1번째 단어), ex) formInputNoRef.current.focus();
 
-// State와 Ref의 차이 = State는 바로 값이 변하지만, Ref는 다시 랜더링이 되어야만 변해진다.
+// State와 Ref의 차이 = State는 바로 값이 변하지만, Ref는 다시 랜더링이 되어야만 변해진다
